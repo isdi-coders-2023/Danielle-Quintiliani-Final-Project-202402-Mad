43df425860a6eba02ab49ce9aac334d0b0970596
malethik <quintiliani.d@gmail.com>
2024-05-06 15:34:01 +0200

    update testing and resolve login method

--- src/app/core/state/state.service.spec.ts
+++ src/app/core/state/state.service.spec.ts
@@ -1,16 +1,79 @@
-import { TestBed } from '@angular/core/testing';
-
-import { StateService } from './state.service';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { Payload, StateService } from './state.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
+import { RepoService } from '../repo/repo.service';
+import { jwtDecode } from 'jwt-decode';
 
 describe('StateService', () => {
   let service: StateService;
+  let mockServerService: jasmine.SpyObj<RepoService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockServerService = jasmine.createSpyObj('ServerService', ['getById']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        StateService,
+        { provide: RepoService, useValue: mockServerService },
+      ],
+    });
     service = TestBed.inject(StateService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should return state observable with getState', () => {
+    const state = service.getState();
+    expect(state).toBeInstanceOf(Observable);
+  });
+  it('should return token if available with getToken', () => {
+    const token = 'myToken';
+    service.state$.next({ ...service.state$.value, token });
+    expect(service.getToken()).toEqual(token);
+  });
+
+  it('should setLoginState', () => {
+    const loginState = 'logged';
+    service.setLoginState(loginState);
+    service.getState().subscribe((state) => {
+      expect(state.loginState).toEqual(loginState);
+    });
+  });
+  it('should setlogin', fakeAsync(() => {
+    /*    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsdnVzbG1tbDAwMDBvaHpsMWE4dGJuOXEiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTcxNDk4OTc3NX0.arK_3EXtcYJfe3J9FZB-haFjnsRm3yloBYMq0tTzkkM';
+ */
+    const token = 'my token';
+    const user = {
+      id: '123',
+      name: 'Test User',
+      email: 'test@example.com',
+      password: 'password',
+      birthday: '11/01/1986',
+      avatar: null,
+      role: 'USER' as 'USER' | 'ADMIN' | 'CLUB',
+    };
+    const payload: Payload = {
+      id: user.id,
+      role: user.role,
+      iat: 1234567890,
+    };
+
+    spyOn(localStorage, 'setItem').and.stub();
+    spyOn(service, 'jwt').and.returnValue(payload);
+
+    mockServerService.getById.and.returnValue(of(user));
+
+    service.setLogin(token);
+    tick();
+
+    expect(service.state$.value.loginState).toEqual('logged');
+    expect(service.state$.value.token).toEqual(token);
+    expect(service.state$.value.currenPayload).toEqual(payload);
+    expect(service.state$.value.currenUser).toEqual(user);
+  }));
 });
--- src/app/main/login/login.component.spec.ts
+++ src/app/main/login/login.component.spec.ts
@@ -1,15 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import LoginComponent from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RepoService } from '../../core/repo/repo.service';
+import { StateService } from '../../core/state/state.service';
+import { Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockRepoService: jasmine.SpyObj<RepoService>;
+  let mockStateService: StateService;
+  let mockRouter: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent],
+      imports: [LoginComponent, HttpClientTestingModule, ReactiveFormsModule],
+      providers: [
+        { provide: RepoService, useValue: mockRepoService },
+        { provide: StateService, useValue: mockStateService },
+        { provide: Router, useValue: mockRouter },
+      ],
     }).compileComponents();
+    mockRepoService = jasmine.createSpyObj('RepoService', ['login']);
+    mockStateService = jasmine.createSpyObj('StateService', [
+      'setLogin',
+      'setLoginState',
+    ]);
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -19,4 +44,44 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should submit login form successfully', fakeAsync(() => {
+    const token = 'exampleToken';
+    const loginFormData = {
+      name: 'test',
+      email: 'test@example.com',
+      password: 'password',
+    };
+    const loginResponse = { token };
+
+    mockRepoService.login.and.returnValue(of(loginResponse));
+
+    component.formLogin.setValue(loginFormData);
+
+    component.submit();
+    tick();
+
+    expect(mockRepoService.login).toHaveBeenCalledWith(loginFormData);
+    expect(mockStateService.setLogin).toHaveBeenCalledWith(token);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/home']);
+  }));
+
+  it('should handle login error', fakeAsync(() => {
+    const errorMessage = 'Invalid credentials' as unknown as Error;
+    const loginFormData = {
+      name: 'test',
+      email: 'test@example.com',
+      password: 'password',
+    };
+    mockRepoService.login(loginFormData);
+    mockRepoService.login.and.throwError(errorMessage);
+
+    component.formLogin.setValue(loginFormData);
+
+    component.submit();
+    tick();
+
+    expect(mockRepoService.login).toHaveBeenCalledWith(loginFormData);
+    expect(mockStateService.setLoginState).toHaveBeenCalledWith('error');
+    expect(console.error).toThrowError('Invalid credentials');
+  }));
 });
