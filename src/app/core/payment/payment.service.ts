135b74f7d4ee7a53453874902a4c9aefd229893e
malethik <quintiliani.d@gmail.com>
2024-05-18 21:42:20 +0200

    fixed payment url

--- src/app/core/payment/payment.service.ts
+++ src/app/core/payment/payment.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, inject } from '@angular/core';
-import { environment } from '../../../environment/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -10,12 +9,11 @@ export interface CreatePaymentIntentResponse {
   providedIn: 'root',
 })
 export class PaymentService {
-  private apiUrl = environment;
   http = inject(HttpClient);
 
   createPaymentIntent(amount: number): Observable<{ clientSecret: string }> {
     return this.http.post<{ clientSecret: string }>(
-      `http://localhost:3400/payment/create-payment-intent`,
+      `https://danielle-quintiliani-final-project.onrender.com/payment/create-payment-intent`,
       { amount },
     );
   }
